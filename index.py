804fefa01faa3d2f0bc5e0c49a17a4b72dc9daef
mam477 <fruscosomam@hotmail.com>
2025-03-18 22:17:25 -0400

    Add files via upload

---
+++ index.py
@@ -0,0 +1,44 @@
+print("hello, world")
+
+if 5 > 2:
+    print("cinco é maior que dois!")
+    x=5
+    y="hello, word"
+    z=2
+    print(x,y,z)
+
+    print(x+z)
+    x = 5
+    y = 10
+
+    print(type(x))
+    print(type(y))
+
+    x,y,z = "Laranja","Banana","Maça"
+    print(x,y,z)
+    x = 5 
+    y = 10
+    print(x+y)
+    x = "maravilhoso"
+
+    def myfunc():
+        x = "dificil"
+        print("python is" + x)
+    def myfunc():
+        global x 
+        x = 'fantastic'
+    myfunc()
+    print("python is" + x)
+    x = "awesome"
+    def myfunc():
+        global x
+        x = "fantastic"
+    myfunc()
+
+    print("python is" + x) 
+    price = 50
+
+    txt = f"the price is {price:.2f} dollars"
+
+    print(txt)
+
